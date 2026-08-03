75ed871a0616f82e224e4428c50386f9ba7bb33a
ASHUTOSH KUMAR <ashutosh@socialconnect.com>
2026-08-03 14:18:02 +0530

    Simplify config.js for single-instance Render deployment (same-origin, no cross-origin complexity)

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,19 +1,4 @@
 (function () {
-  var RENDER_BACKEND = 'https://socialconnect-g0it.onrender.com';
-  var RENDER_HOST = 'socialconnect-g0it.onrender.com';
-  var CUSTOM_DOMAINS = ['biko.work.gd', 'www.biko.work.gd'];
-  var isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
-  var isSameOriginAsBackend = isLocal ||
-    location.hostname === RENDER_HOST ||
-    CUSTOM_DOMAINS.includes(location.hostname);
-
-  if (isSameOriginAsBackend) {
-    window.API_BASE = '';
-    window.SOCKET_URL = undefined;
-  } else {
-    window.API_BASE = RENDER_BACKEND;
-    window.SOCKET_URL = RENDER_BACKEND;
-  }
-
-  window.SOCKET_IO_VERSION = '4.6.1';
+  window.API_BASE = '';
+  window.SOCKET_URL = undefined;
 })();
